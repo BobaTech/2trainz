99d1ce8498514598298cc3db8f01bece7761984c
cajunazn7 <kevinw326@gmail.com>
2015-07-15 02:22:10 +0000

    add railroad

--- js/map.js
+++ js/map.js
@@ -7,10 +7,18 @@ var initialize = function() {
     };
     google.maps.visualRefresh = false;
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    map.data.loadGeoJson('data/casualties.geo.json');
+    map.data.loadGeoJson("data/casualties.geo.json");
+    map.data.loadGeoJson("data/us-railroads-10m.json");
 };
 
 $(document).ready(function() {
     google.maps.event.addDomListener(window, "load", initialize);
     console.log("done");
 });
+
+/*$.getJSON("data/us-railroads.json", function(data){
+	
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	map.data.addGeoJson(geoJsonObject); 
+}); */
+      
