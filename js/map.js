62edb903fbef527ce2388b368e1439882f5f1233
cajunazn7 <kevinw326@gmail.com>
2015-07-16 05:18:04 +0000

    limiting map movement

--- js/map.js
+++ js/map.js
@@ -1,10 +1,45 @@
 "use strict";
 
 var initialize = function() {
+	var minZoomLevel = 3;
     var mapOptions = {
         center: { lat: 39.0, lng: -98 },
-        zoom: 3
+        zoom: minZoomLevel
     };
+    var allowedBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(39.368149, -65.915875), 
+    new google.maps.LatLng(39.368149, -127.798348)
+	);
+	
+	google.maps.event.addListener(map, 'dragend', function() {
+	    if (allowedBounds.contains(map.getCenter())) return;
+	    var center = map.getCenter(),
+	        x = center.lng(),
+	        y = center.lat(),
+	        maxX = allowedBounds.getNorthEast().lng(),
+	        maxY = allowedBounds.getNorthEast().lat(),
+	        minX = allowedBounds.getSouthWest().lng(),
+	        minY = allowedBounds.getSouthWest().lat();
+	
+	    if (x < minX){
+	    	x = minX;
+	    }
+	    if (x > maxX){
+	    	x = maxX;	
+	    } 
+	    if (y < minY){
+	    	y = minY;
+	    } 
+	    if (y > maxY){ 
+	    	y = maxY;
+	    }
+	    map.setCenter(new google.maps.LatLng(y, x));
+	});
+	
+	google.maps.event.addListener(map, 'zoom_changed', function() {
+	   if (map.getZoom() < minZoomLevel) map.setZoom(minZoomLevel);
+	});
+
     google.maps.visualRefresh = false;
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     map.data.loadGeoJson("data/casualties.geo.json");
@@ -16,9 +51,11 @@ $(document).ready(function() {
     console.log("done");
 });
 
+
 /*$.getJSON("data/us-railroads.json", function(data){
 	
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	map.data.addGeoJson(geoJsonObject); 
 }); */
       
+
